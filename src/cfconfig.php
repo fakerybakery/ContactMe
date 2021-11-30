1f556cf839791e0543a061a755f3aab1088fa61f
Fakery Bakery <76186054+fakerybakery@users.noreply.github.com>
2021-11-29 17:11:33 -1000

    Update cfconfig.php

--- src/cfconfig.php
+++ src/cfconfig.php
@@ -1,6 +1,6 @@
 <?php
 $your_email = ''; // <-- Enter the email to send messages to here! NOTE: This email address must be valid. NOTE: You must have PHP Mail function installed on your computer.
-$contact_form_name ''; // <-- e.g. Example.com Contact Form
+$contact_form_name = ''; // <-- e.g. Example.com Contact Form
 /*
 It is illegal, unethical, and immoral to remove credit from this contact form.
 This contact form is powered by MrFakeName.
